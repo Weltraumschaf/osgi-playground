65c76481ea6a5e608f93973d1317173e504d1d62
Sven Strittmatter <sven.strittmatter@iteratec.de>
2015-11-03 22:13:20 +0100

    Add script to clean setup karaf.

---
+++ setup-karaf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TARBALL="apache-karaf-3.0.5.tar.gz"
+TARGET="apache-karaf-3.0.5"
+SHORTHAND="karaf"
+
+if [ -d "${TARGET}" ] ; then 
+    rm -rfv "${TARGET}"
+fi
+
+if [ -e "${SHORTHAND}" ] ; then
+    rm -v "${SHORTHAND}"
+fi
+
+tar xjvf "${TARBALL}"
+ln -s "${TARGET}" "${SHORTHAND}"
+
+cd "${SHORTHAND}/etc"
+ln -s "../../de.weltraumschaf.nas.ui.cfg"
+rm -v org.ops4j.pax.logging.cfg
+ln -s "../../org.ops4j.pax.logging.cfg"
+cd -
